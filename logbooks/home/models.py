d658b19459e8dbc77a1a69f322ef9b37969debcb
Aryan basu <122888041+aryan-basu-gemini@users.noreply.github.com>
2023-05-19 14:34:37 +0530

    second commit

--- logbooks/home/models.py
+++ logbooks/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=200)
+    description=models.TextField()
+    amount=models.IntegerField()
+    def _str_(self):
+        return self.name
--- logbooks/home/views.py
+++ logbooks/home/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import Contact
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    data=Contact.objects.all()
+    main_data={
+    'data':data
+    }
+    print(main_data)
+    #print(main_data)
+    if request.method=="POST":
+        contact=Contact()
+        name=request.POST.get('name')
+        description=request.POST.get('description')
+        amount=request.POST.get('amount')
+        contact.name=name
+        contact.amount=amount
+        contact.description=description
+        contact.save()
+      
+   # print(data.name)
+       
+        return render(request,'index.html',main_data)
+        #return HttpResponse("<h1>Thanks for contact us</h1>")
+        
+    return render(request,'index.html',main_data)
